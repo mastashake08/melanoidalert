23424317d267ccb8a6e022b77f608e87332c1a9a
Jyrone Parker <jyrone.parker@gmail.com>
2017-04-02 11:28:24 -0400

    email

---
+++ resources/views/suggestion.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Have a suggestion?</div>
+
+                <div class="panel-body">
+                  Send me an email at
+<a href="mailto:jyrone.parker@gmail.com">jyrone.parker@gmail.com</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
